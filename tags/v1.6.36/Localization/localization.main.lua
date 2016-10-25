4c30ca32d6a339cc53519263b2b72a394ec97b05
xit_draka <xit_draka@74ff3c2d-be9d-401d-913b-ac46c53d266c>
2016-10-25 16:24:23 +0000

    Tagging as v1.6.36

---
+++ tags/v1.6.36/Localization/localization.main.lua
@@ -0,0 +1,26 @@
+-- English text strings
+
+Localization.RegisterAddonStrings("enUS", "Localization",
+{
+    -- Locales
+    enUS      = "English",
+    enGB      = "English",
+    frFR      = "French",
+    ruRU      = "Russian",
+    zhTW      = "Traditional Chinese",
+    deDE      = "German",
+    ptBR      = "Brazilian Portuguese",
+
+    -- Frame Strings
+    Confirm         = "Confirm",
+    Cancel          = "Cancel",
+    RemoveUnused    = "Remove unused locales from memory.",
+    SelectPreferred = "Select a preferred localization:",
+    AvailLangs      = "Available Languages",
+
+    --Earth Button
+    Localization  = "Localization",
+    ShowPrompt    = "Show Prompt",
+    EarthTooltip  = "Show Locale Selection Dialogue.",
+},
+nil, true) -- Protected
